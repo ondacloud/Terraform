2ddb6569008c450ab5207318e78fe5d2bf57cca0
ondacloud <junseong8299@gmail.com>
2024-01-18 19:00:05 +0900

    Terraform

---
+++ DynamoDB/Dynamodb.tf
@@ -0,0 +1,24 @@
+resource "aws_dynamodb_table" "dynamodb" {
+    name = "<dynamodb>"
+    billing_mode   = "PAY_PER_REQUEST" # PAY_PER_REQUEST or PROVISIONED
+    hash_key = "<hash_key>"
+    # range_key = "<range_key>"
+    
+    # attribute {
+    #     name = "<Name>"
+    #     type = "S"
+    # }
+
+    # attribute {
+    #     name = "<Name>"
+    #     type = "N"
+    # }
+
+    tags = {
+        Name = "<dynamodb>"
+    }
+}
+
+output "dynamodb" {
+    value = aws_dynamodb_table.dynamodb.name
+}
