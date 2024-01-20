f11806e827d521a185cd790886a1a053094e38c1
ondacloud <junseong8299@gmail.com>
2024-01-20 14:33:55 +0900

    Real_Time_Service

--- SampleCode-VPC&EC2/2Subnet-3AZ.tf
+++ SampleCode-VPC&EC2/2Subnet-3AZ.tf
@@ -226,6 +226,22 @@ data "aws_ssm_parameter" "latest_ami" {
   name = "/aws/service/ami-amazon-linux-latest/al2023-ami-minimal-kernel-default-x86_64"
 }
 
+## Keypair
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
+resource "aws_key_pair" "keypair" {
+  key_name = "<env>"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "local_file" "keypair" {
+  content = tls_private_key.rsa.private_key_pem
+  filename = "../<env>.pem"
+}
+
 ## Public EC2
 resource "aws_instance" "bastion" {
   ami = data.aws_ssm_parameter.latest_ami.value
