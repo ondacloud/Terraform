d68cf16945eab4c858ee908285d0d23d3abebd93
ondacloud <junseong8299@gmail.com>
2024-01-21 10:18:10 +0900

    Terraform

--- EC2/ec2-basic.tf
+++ EC2/ec2-basic.tf
@@ -1,4 +1,9 @@
-## EC2
+# EC2
+## AMI
+data "aws_ssm_parameter" "latest_ami" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-minimal-kernel-default-x86_64"
+}
+
 resource "aws_instance" "bastion" {
   ami = data.aws_ssm_parameter.latest_ami.value
   subnet_id = aws_subnet.public_a.id
