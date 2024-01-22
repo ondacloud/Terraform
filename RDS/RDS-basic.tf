736923772923cf713e497c1fa4d093ff3df7b59e
ondacloud <junseong8299@gmail.com>
2024-01-22 16:55:34 +0900

    Terraform

--- RDS/RDS-basic.tf
+++ RDS/RDS-basic.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "db" {
     name        = "<env>-EC2-SG"
     description = "<env>-EC2-SG"
-    vpc_id      = aws_vpc.vpc.id
+    vpc_id      = aws_vpc.main.id
 
     ingress {
         protocol = "tcp"
