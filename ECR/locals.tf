2b5437242a1d92ccce1f64f82acdc627a0a42470
ondacloud <junseong8299@gmail.com>
2025-10-02 11:48:03 +0900

    fest: Add Terraform Module ECR

---
+++ ECR/locals.tf
@@ -0,0 +1,33 @@
+locals {
+  parameter = "demo"
+}
+
+locals {
+  kmss = {
+    "${local.parameter}/ecr/kms" = {
+      alias_name              = "alias/${local.parameter}/ecr/kms"
+      key_usage               = "ENCRYPT_DECRYPT"
+      deletion_window_in_days = 7
+    }
+  }
+}
+
+locals {
+  ecrs = {
+    "${local.parameter}-ecr" = {
+      image_tag_mutability              = "IMMUTABLE" # IMMUTABLE or MUTABLE
+      force_delete                      = true
+      scan_images_on_push               = true
+
+      enable_kms                        = true
+      kms_key_name                      = "${local.parameter}/ecr/kms"
+      encryption_type                   = "KMS"
+
+      # Use IMMUTABLE_WITH_EXCLUSION or MUTABLE_WITH_EXCLUSION for image_tag_mutability when using enable_image_tag_exclusion_filter
+      enable_image_tag_exclusion_filter = false
+      image_tag_exclusion_filter = [
+        {filter = "latest", filter_type = "WILDCARD"},
+      ]
+    }
+  }
+}
---
+++ ECR/main.tf
@@ -0,0 +1,28 @@
+module "kms" {
+  source = "./modules/kms"
+
+  for_each = local.kmss
+
+  name                    = each.key
+  alias_name              = each.value.alias_name
+  key_usage               = each.value.key_usage
+  deletion_window_in_days = each.value.deletion_window_in_days
+}
+
+module "ecr" {
+  source = "./modules/ecr"
+
+  for_each = local.ecrs
+
+  name                     = each.key
+  image_tag_mutability     = each.value.image_tag_mutability
+  force_delete             = each.value.force_delete
+  scan_images_on_push      = each.value.scan_images_on_push
+
+  encryption_configuration = each.value.enable_kms ? {
+    encryption_type = each.value.encryption_type
+    kms_key = module.kms[each.value.kms_key_name].kms_arn
+    # kms_key = null
+  } : null
+  image_tag_mutability_exclusion_filter = each.value.enable_image_tag_exclusion_filter ? each.value.image_tag_exclusion_filter : []
+}
