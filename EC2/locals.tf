3566b4a0912e33474a857beb75915f5de1252053
ondacloud <junseong8299@gmail.com>
2025-10-11 21:04:59 +0900

    fix: Change to Module VPC

--- EC2/locals.tf
+++ EC2/locals.tf
@@ -13,7 +13,13 @@ locals {
   vpcs = {
     "${local.parameter}-vpc" = {
       vpc_cidr         = "10.0.0.0/16"
-      default_rtb_name = "${local.parameter}-default-rtb"
+      default_rtb_tags = {
+        Name = "${local.parameter}-default-rtb"
+      }
+
+      vpc_tags = {
+        Name        = "${local.parameter}-vpc"
+      }
 
       enable_igw       = true
       enable_natgw     = true
@@ -21,18 +27,40 @@ locals {
       types = [
         {
           type         = "public"
-          sn_name      = "${local.parameter}-public-$1"
           sn_cidrs     = ["10.0.0.0/24", "10.0.1.0/24"]
-          rtb_name     = "${local.parameter}-public-rtb"
-          igw_name     = "${local.parameter}-igw"
+          sn_tags      = {
+            Name = "${local.parameter}-public-$1"
+            environment = "demo"
+          }
+
+          rtb_tags     = {
+            Name = "${local.parameter}-public-rtb"
+            environment = "demo"
+          }
+
+          igw_tags     = {
+            Name = "${local.parameter}-igw"
+            environment = "demo"
+          }
         },
         {
           type         = "private"
-          sn_name      = "${local.parameter}-private-$1"
           sn_cidrs     = ["10.0.2.0/24", "10.0.3.0/24"]
-          rtb_name     = "${local.parameter}-private-$1-rtb"
-          natgw_name   = "${local.parameter}-natgw-$1"
-        }
+          sn_tags      = {
+            Name = "${local.parameter}-private-$1"
+            environment = "demo"
+          }
+
+          rtb_tags     = {
+            Name       = "${local.parameter}-private-$1-rtb"
+            environment = "demo"
+          }
+
+          natgw_tags   = {
+            Name = "${local.parameter}-natgw-$1"
+            environment = "demo"
+          }
+        },
       ]
     }
   }
