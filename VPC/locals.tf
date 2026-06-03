64f24ecbdb878a11b5725c01f88f0094742203b7
ondacloud <junseong8299@gmail.com>
2026-06-03 23:58:45 +0900

    fix: Modify of Module VPC

--- VPC/locals.tf
+++ VPC/locals.tf
@@ -13,69 +13,45 @@ locals {
   vpcs = {
     "${local.parameter}-vpc" = {
       vpc_cidr         = "10.0.0.0/16"
-      default_rtb_tags = {
-        Name = "${local.parameter}-default-rtb"
-      }
+      default_rtb_tags = {Name = "${local.parameter}-default-rtb"}
 
-      vpc_tags = {
-        Name        = "${local.parameter}-vpc"
-      }
+      vpc_tags = {Name = "${local.parameter}-vpc"}
 
-      enable_igw       = true
-      enable_natgw     = true
+      enable_igw       = false
+      enable_natgw     = false
 
       types = [
         {
           type         = "public"
           sn_cidrs     = ["10.0.0.0/24", "10.0.1.0/24"]
-          sn_tags      = {
-            Name = "${local.parameter}-public-$1"
-          }
+          sn_tags      = {Name = "${local.parameter}-public-$1"}
 
-          rtb_tags     = {
-            Name = "${local.parameter}-public-rtb"
-          }
+          rtb_tags     = {Name = "${local.parameter}-public-rtb"}
 
-          igw_tags     = {
-            Name = "${local.parameter}-igw"
-          }
+          igw_tags     = {Name = "${local.parameter}-igw"}
         },
         {
           type         = "private"
           sn_cidrs     = ["10.0.2.0/24", "10.0.3.0/24"]
-          sn_tags      = {
-            Name = "${local.parameter}-private-$1"
-          }
+          sn_tags      = {Name = "${local.parameter}-private-$1"}
 
-          rtb_tags     = {
-            Name       = "${local.parameter}-private-$1-rtb"
-          }
+          rtb_tags     = {Name       = "${local.parameter}-private-$1-rtb"}
 
-          natgw_tags   = {
-            Name = "${local.parameter}-natgw-$1"
-          }
+          natgw_tags   = {Name = "${local.parameter}-natgw-$1"}
         },
         {
           type         = "protect"
           sn_cidrs     = ["10.0.4.0/24", "10.0.5.0/24"]
-          sn_tags      = {
-            Name = "${local.parameter}-protect-$1"
-          }
+          sn_tags      = {Name = "${local.parameter}-protect-$1"}
 
-          rtb_tags     = {
-            Name = "${local.parameter}-protect-rtb"
-          }
+          rtb_tags     = {Name = "${local.parameter}-protect-rtb"}
         },
         {
           type         = "inspect"
           sn_cidrs     = ["10.0.6.0/24", "10.0.7.0/24"]
-          sn_tags      = {
-            Name = "${local.parameter}-inspect-$1"
-          }
+          sn_tags      = {Name = "${local.parameter}-inspect-$1"}
 
-          rtb_tags     = {
-            Name = "${local.parameter}-inspect-$1-rtb"
-          }
+          rtb_tags     = {Name = "${local.parameter}-inspect-$1-rtb"}
         }
       ]
     }
