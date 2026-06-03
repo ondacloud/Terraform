658d9b8b3c0f346e65d0b0c45ce72b3cc5ad91a7
ondacloud <junseong8299@gmail.com>
2026-06-04 00:31:48 +0900

    fix: Modify of Module KMS

--- KMS/locals.tf
+++ KMS/locals.tf
@@ -5,13 +5,20 @@ locals {
 locals {
   kmss = {
     "${local.parameter}/kms" = {
-      tags = {
-        Name = "${local.parameter}/kms"
-      }
+      tags = {Name = "${local.parameter}/kms"}
       
       alias_name              = "alias/${local.parameter}/kms"
       key_usage               = "ENCRYPT_DECRYPT"
       deletion_window_in_days = 7
-    }
+      statements = [
+        {
+          effect = "Allow"
+          principals = {type = "AWS", identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]}
+          actions   = ["kms:*"]
+          resources = ["*"]
+          conditions = []
+        },
+      ]
+    },
   }
 }
--- KMS/modules/kms/variables.tf
+++ KMS/modules/kms/variables.tf
@@ -17,3 +17,24 @@ variable "key_usage" {
 variable "deletion_window_in_days" {
   type = number
 }
+
+variable "statements" {
+  type = list(object({
+    sid    = optional(string)
+    effect = string
+
+    principals = optional(object({
+      type        = string
+      identifiers = list(string)
+    }))
+
+    actions   = list(string)
+    resources = list(string)
+
+    conditions = optional(list(object({
+      test     = string
+      variable = string
+      values   = list(string)
+    })), [])
+  }))
+}
