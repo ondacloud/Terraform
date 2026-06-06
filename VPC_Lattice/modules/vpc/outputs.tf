390cd3b5a2ade895fa7dbd2ce6ffea35442239a9
ondacloud <junseong8299@gmail.com>
2026-06-06 21:30:08 +0900

    fix: Modify of Module VPC Lattice

--- VPC_Lattice/modules/vpc/outputs.tf
+++ VPC_Lattice/modules/vpc/outputs.tf
@@ -1,115 +1,47 @@
-output "vpc_id" {
-  value = aws_vpc.this.id
+variable "az_override" {
+  type = list(string)
 }
 
-output "vpc_cidr" {
-  value = aws_vpc.this.cidr_block
+variable "azs" {
+  type = list(string)
 }
 
-output "public_subnet_ids" {
-  value = [
-    for k, s in aws_subnet.this :
-    s.id if lookup(local.types[k], "type", "") == "public"
-  ]
+variable "enable_igw" {
+  type = string
 }
 
-output "private_subnet_ids" {
-  value = [
-    for k, s in aws_subnet.this :
-    s.id if lookup(local.types[k], "type", "") == "private"
-  ]
+variable "enable_natgw" {
+  type = string
 }
 
-output "protect_subnet_ids" {
-  value = [
-    for k, s in aws_subnet.this :
-    s.id if lookup(local.types[k], "type", "") == "protect"
-  ]
+variable "default_rtb_tags" {
+  type = map(string)
 }
 
-output "inspect_subnet_ids" {
-  value = [
-    for k, s in aws_subnet.this :
-    s.id if lookup(local.types[k], "type", "") == "inspect"
-  ]
+variable "default_sg_tags" {
+  type = map(string)
 }
 
-output "public_route_table_ids" {
-  value = [
-    for k, rt in aws_route_table.this :
-    rt.id if lookup(local.rtbs_to_create[k], "type", "") == "public"
-  ]
+variable "vpc_name" {
+  type = string
 }
 
-output "protect_route_table_ids" {
-  value = [
-    for k, rt in aws_route_table.this :
-    rt.id if lookup(local.rtbs_to_create[k], "type", "") == "protect"
-  ]
+variable "vpc_tags" {
+  type = map(string)
 }
 
-output "private_route_table_ids" {
-  value = [
-    for k, rt in aws_route_table.this :
-    rt.id if lookup(local.rtbs_to_create[k], "type", "") == "private"
-  ]
+variable "vpc_cidr" {
+  type = string
 }
 
-output "inspect_route_table_ids" {
-  value = [
-    for k, rt in aws_route_table.this :
-    rt.id if lookup(local.rtbs_to_create[k], "type", "") == "inspect"
-  ]
-}
+variable "types" {
+  type = list(object({
+    type     = string
+    sn_cidrs = list(string)
+    sn_tags  = map(string)
+    rtb_tags = map(string)
 
-
-output "igw_id" {
-  value = var.enable_igw ? aws_internet_gateway.this[0].id : null
-}
-
-output "natgw_ids" {
-  value = values(try({ for k, ngw in aws_nat_gateway.this : k => ngw.id }, {}))
-}
-
-output "public_subnet_azs" {
-  value = values(try({ for k, s in aws_subnet.this : k => s.availability_zone if s.type == "public" }, {}))
-}
-
-output "private_subnet_azs" {
-  value = values(try({ for k, s in aws_subnet.this : k => s.availability_zone if s.type == "private" }, {}))
-}
-
-output "protect_subnet_azs" {
-  value = values(try({ for k, s in aws_subnet.this : k => s.availability_zone if s.type == "protect" }, {}))
-}
-
-output "inspect_subnet_azs" {
-  value = values(try({ for k, s in aws_subnet.this : k => s.availability_zone if s.type == "inspect" }, {}))
-}
-
-output "subnet_ids" {
-  value = {
-    for k, sn in aws_subnet.this :
-    "${sn.tags["Name"]}" => sn.id
-  }
-}
-
-output "subnet_ids_by_type" {
-  value = {
-    for t in distinct([for v in local.types : v.type]) :
-    t => [
-      for k, s in aws_subnet.this :
-      s.id if local.types[k].type == t
-    ]
-  }
-}
-
-output "route_table_ids_by_type" {
-  value = {
-    for t in distinct([for v in local.types : v.type]) :
-    t => [
-      for k, rt in aws_route_table.this :
-      rt.id if local.rtbs_to_create[k].type == t
-    ]
-  }
+    igw_tags   = optional(map(string))
+    natgw_tags = optional(map(string))
+  }))
 }
